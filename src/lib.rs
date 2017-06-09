fda8cf121a1e2da06959f3185e3dd9bfdec64234
Sean McArthur <sean.monstar@gmail.com>
2017-06-09 14:33:55 -0700

    v0.3.1

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/mime/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/mime/0.3.1")]
 #![deny(warnings)]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
