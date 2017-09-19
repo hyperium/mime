0530c2be902bd58f2ad2b2ccb8df8dae99728ac4
Sean McArthur <sean.monstar@gmail.com>
2017-09-19 10:41:10 -0700

    v0.3.4

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/mime/0.3.3")]
+#![doc(html_root_url = "https://docs.rs/mime/0.3.4")]
 #![deny(warnings)]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
